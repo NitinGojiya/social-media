6e3b1355f9d2ee2b9c5769708faeb30bc2d30dec
nitin <nitingojiya2000@gmail.com>
2025-08-12 15:23:47 +0530

    done ci/cd 2

--- test/controllers/home_pages_controller_test.rb
+++ test/controllers/home_pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class HomePagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_pages_index_url
+    get root_path
     assert_response :success
   end
 end
